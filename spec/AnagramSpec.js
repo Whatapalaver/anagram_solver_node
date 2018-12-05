3c98d3c316371502bec22ef75d087263f24da6c1
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-05 18:49:08 +0000

    Creating a lookup object from dictionary. Ordered word with array of anagrams

--- spec/AnagramSpec.js
+++ spec/AnagramSpec.js
@@ -1,25 +1,41 @@
 
 describe("Anagram", function() {
   const Anagram = require('../src/Anagram.js');
-  let newWord;
+  let newAnagram;
   let word1 = 'chopsticks';
   let hyphenWord = 'chop-sticks';
   let capitalisedWord = 'Chopsticks';
+  const testWordlist = ['zone', 'zone', 'act', 'cat', 'tac', 'art', 'rat', 'tar', 'spar', 'rasp', 'pars', 'raps', 'shed']
+  const groupedWordlist = { enoz: [ 'zone', 'zone' ],
+                            act: [ 'act', 'cat', 'tac' ],
+                            art: [ 'art', 'rat', 'tar' ],
+                            aprs: [ 'spar', 'rasp', 'pars', 'raps' ],
+                            dehs: [ 'shed' ] 
+                          }
 
   beforeEach(function() {
-    newWord = new Anagram(word1);
+    newAnagram = new Anagram(word1);
   });
 
-  it("should be able to put word in alphabetical order", function() {
-    expect(newWord.alphabeticOrder(word1)).toEqual('cchikopsst');
-  });
+  describe("alphabeticOrder", function() {
+    it("should be able to put word in alphabetical order", function() {
+      expect(newAnagram.alphabeticOrder(word1)).toEqual('cchikopsst');
+    });
+  
+    it("should ignore non alphanumeric characters", function() {
+      expect(newAnagram.alphabeticOrder(hyphenWord)).toEqual('cchikopsst');
+    });
+  
+    it("should downcase all characters", function() {
+      expect(newAnagram.alphabeticOrder(capitalisedWord)).toEqual('cchikopsst');
+    });
+  })
 
-  it("should ignore non alphanumeric characters", function() {
-    expect(newWord.alphabeticOrder(hyphenWord)).toEqual('cchikopsst');
-  });
-
-  it("should downcase all characters", function() {
-    expect(newWord.alphabeticOrder(capitalisedWord)).toEqual('cchikopsst');
-  });
+  describe("dictionaryGrouper", function() {
+    it("creates an object of ordered keys with associated array of anagrams", function() {
+      expect(newAnagram.dictionaryGrouper(testWordlist)).toEqual(groupedWordlist);
+    });
+  })
+ 
 
 });
--- src/Anagram.js
+++ src/Anagram.js
@@ -1,16 +1,33 @@
+const tempDictionary = ['zone', 'zone', 'act', 'cat', 'tac', 'art', 'rat', 'tar', 'spar', 'rasp', 'pars', 'raps', 'shed']
+
 function Anagram(word) {
   this.word = word;
   this.anagrams = [];
 }
 
-Anagram.prototype.alphabeticOrder = (word) => {
-  if (!word) { return; }
-  sortedWord = word.replace(/\W/g, '').toLowerCase().split('').sort().join('');
-  return sortedWord;
+Anagram.prototype.dictionaryGrouper = function(wordlist) {
+  const groupedDictionary = {};
+  wordlist.forEach((word) => {
+    const orderedWord = this.alphabeticOrder(word);
+    if (groupedDictionary[orderedWord]) {
+      return groupedDictionary[orderedWord].push(word);
+    }
+    groupedDictionary[orderedWord] = [word];
+  });
+  return groupedDictionary;
 }
+
+Anagram.prototype.alphabeticOrder = function(word) {
+  if (!word) { return; }
+    orderedWord = word.replace(/\W/g, '').toLowerCase().split('').sort().join('');
+  return orderedWord;
+};
 
 
 // test = new Anagram('')
-// console.log(test.alphabeticOrder("Angela's coat"))
+// group = test.dictionaryGrouper(tempDictionary)
+
+// console.log(group)
+// console.log(group['enoz'])
 
 module.exports = Anagram;
