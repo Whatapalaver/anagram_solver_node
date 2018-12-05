b9dccd102a1a2f4fb4334d1409d61faa3cb98a79
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-05 20:21:40 +0000

    Creating returnAnagram method that return anagram string where they exist

--- spec/AnagramSpec.js
+++ spec/AnagramSpec.js
@@ -3,6 +3,7 @@ describe("Anagram", function() {
   const Anagram = require('../src/Anagram.js');
   let newAnagram;
   let word1 = 'chopsticks';
+  let word2 = 'cat';
   let hyphenWord = 'chop-sticks';
   let capitalisedWord = 'Chopsticks';
   const testWordlist = ['zone', 'zone', 'act', 'cat', 'tac', 'art', 'rat', 'tar', 'spar', 'rasp', 'pars', 'raps', 'shed']
@@ -14,7 +15,7 @@ describe("Anagram", function() {
                           }
 
   beforeEach(function() {
-    newAnagram = new Anagram(word1);
+    newAnagram = new Anagram(word2);
   });
 
   describe("alphabeticOrder", function() {
@@ -35,6 +36,17 @@ describe("Anagram", function() {
     it("creates an object of ordered keys with associated array of anagrams", function() {
       expect(newAnagram.dictionaryGrouper(testWordlist)).toEqual(groupedWordlist);
     });
+  })
+
+  describe("returnAnagram", function() {
+    it("returns a list of valid anagrams", function() {
+      expect(newAnagram.returnAnagram()).toMatch('act, cat, tac');
+    });
+    it("throws an error for words without an anagram", function() {
+      let invalidWord = new Anagram('at')
+      expect(invalidWord.returnAnagram()).toMatch('');
+    });
+
   })
  
 
--- src/Anagram.js
+++ src/Anagram.js
@@ -1,4 +1,4 @@
-const tempDictionary = ['zone', 'zone', 'act', 'cat', 'tac', 'art', 'rat', 'tar', 'spar', 'rasp', 'pars', 'raps', 'shed']
+const tempDictionary = ['zone', 'act', 'cat', 'tac', 'art', 'rat', 'tar', 'spar', 'rasp', 'pars', 'raps', 'shed']
 
 function Anagram(word) {
   this.word = word;
@@ -18,16 +18,30 @@ Anagram.prototype.dictionaryGrouper = function(wordlist) {
 }
 
 Anagram.prototype.alphabeticOrder = function(word) {
-  if (!word) { return; }
-    orderedWord = word.replace(/\W/g, '').toLowerCase().split('').sort().join('');
+  let orderedWord = word.replace(/\W/g, '').toLowerCase().split('').sort().join('');
   return orderedWord;
 };
 
+Anagram.prototype.returnAnagram = function() {
+  let orderedTestWord = this.alphabeticOrder(this.word)
+  let groupedDictionary = this.dictionaryGrouper(tempDictionary)
+  let potentialAnagrams = groupedDictionary[orderedTestWord]
+  if (!potentialAnagrams || potentialAnagrams.length === 1) {
+    console.log(`There are no anagrams for ${this.word}`)
+    return '';
+  } else {
+    console.log('Here are the anagrams: ', potentialAnagrams.join(', '))
+    return potentialAnagrams.join(', ')
+  };
+};
 
-// test = new Anagram('')
+
+
+// test = new Anagram('act')
 // group = test.dictionaryGrouper(tempDictionary)
 
 // console.log(group)
 // console.log(group['enoz'])
+// test.returnAnagram()
 
 module.exports = Anagram;
